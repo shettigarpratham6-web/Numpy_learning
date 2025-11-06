47f1fc62b072402c36be8aaed24bfe5064d262e0
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-11-06 18:36:00 +0530

    first commit

---
+++ practice_3.py
@@ -0,0 +1,4 @@
+import numpy as np
+arr=np.array(42)
+print(arr)
+print(type(arr))
---
+++ practice_5.py
@@ -0,0 +1,10 @@
+# 3-D array
+import numpy as np
+
+arr3d = np.array([
+                  [[1,2,3],[4,5,6]],
+                  [[7,8,9],[10,11,12]]
+                 ])
+
+print(arr3d)
+print(np.shape(arr3d))
