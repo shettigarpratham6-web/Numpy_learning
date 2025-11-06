125eb6fee120d449ab9f25066d17212344e6b052
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-11-06 18:46:31 +0530

    first commit

---
+++ practice_7.py
@@ -0,0 +1,19 @@
+import numpy as np
+arr1=np.array([1,2,3,4,5])
+print(arr1)
+print("\n")
+print(arr1.ndim)
+print("\n")
+
+arr2=np.array([[1,2,3,4],[5,6,7,8]])
+print(arr2)
+print("\n")
+print(arr2.ndim)
+print("\n")
+arr3=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr3)
+print("\n")
+print(arr3.ndim)
+
+
+
